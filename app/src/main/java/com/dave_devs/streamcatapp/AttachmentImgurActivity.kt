de05245fde5997033de9d5a4a2b3259715a34e57
Oreoluwa Odetunde <102271576+LSR07@users.noreply.github.com>
2023-01-21 02:56:32 +0100

    Initial commit

---
+++ app/src/main/java/com/dave_devs/streamcatapp/AttachmentImgurActivity.kt
@@ -0,0 +1,11 @@
+package com.dave_devs.streamcatapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AttachmentImgurActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.attachment_imgur)
+    }
+}
